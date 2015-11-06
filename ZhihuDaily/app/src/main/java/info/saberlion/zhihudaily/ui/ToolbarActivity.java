0e3a58665796daaf9bfeeb5ecaf4f0c2f2213c07
saberlion <fxre@qq.com>
2015-11-06 15:02:17 +0800

    add ToolbarActivity

---
+++ ZhihuDaily/app/src/main/java/info/saberlion/zhihudaily/ui/ToolbarActivity.java
@@ -0,0 +1,72 @@
+package info.saberlion.zhihudaily.ui;
+
+import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.animation.DecelerateInterpolator;
+
+import info.saberlion.zhihudaily.R;
+
+/**
+ * Created by Arthur on 2015/11/6.
+ */
+public abstract class ToolbarActivity extends BaseActivity {
+    protected AppBarLayout mAppBarLayout;
+    protected Toolbar mToolbar;
+    protected boolean mIsHidden = false;
+
+    abstract protected int provideContentViewId();
+
+    protected abstract boolean canBack();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(provideContentViewId());
+
+        mAppBarLayout = (AppBarLayout) findViewById(R.id.app_bar_layout);
+        mToolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+
+
+        if (mToolbar == null || mAppBarLayout == null) {
+            throw new IllegalStateException("No toolbar");
+        }
+
+        setSupportActionBar(mToolbar);
+
+        if (canBack()) {
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    protected void setAppBarAlpha(float alpha) {
+        mAppBarLayout.setAlpha(alpha);
+    }
+
+
+    protected void hideOrShowToolbar() {
+        mAppBarLayout.animate()
+                .translationY(mIsHidden ? 0 : -mAppBarLayout.getHeight())
+                .setInterpolator(new DecelerateInterpolator(2))
+                .start();
+
+        mIsHidden = !mIsHidden;
+    }
+
+
+}
