a2020abca7f86ea080844bec212fa9e422b1e1d8
saberlion <fxre@qq.com>
2015-11-06 15:01:47 +0800

    MainActivity extends BaseActivity

--- ZhihuDaily/app/src/main/java/info/saberlion/zhihudaily/ui/MainActivity.java
+++ ZhihuDaily/app/src/main/java/info/saberlion/zhihudaily/ui/MainActivity.java
@@ -13,7 +13,6 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -31,7 +30,7 @@ import info.saberlion.zhihudaily.R;
 import info.saberlion.zhihudaily.utils.IntentUtils;
 import info.saberlion.zhihudaily.utils.ToastUtils;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
 
@@ -40,11 +39,14 @@ public class MainActivity extends AppCompatActivity
     DatePagerAdapter datePagerAdapter;
     TabLayout tabLayout;
 
+    protected int provideContentViewId() {
+        return R.layout.activity_main;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i(TAG, "onCreate : ");
-        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
